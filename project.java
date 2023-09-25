b459913bd7b0eba8273945c1d9d256343a9e573d
Matt Ferretti <Ferretti7@yahoo.com>
2023-09-25 13:45:31 -0400

    Create File

---
+++ project.java
@@ -0,0 +1,36 @@
+/************************************************************************************
+ * Build a foundation to understanding the halting problem. 
+ * For the DFA assume that the alphabet is {000, 001, 002, . . . , 997, 998, 999}. 
+ * This is an alphabet with 1000 letters and each single letter is three digits long
+ ************************************************************************************/
+
+/*************************************************************************************
+Using your favorite programming language, implement a deterministic finite automaton M 
+that recognizes the language:
+   
+L1 = {w|w contains a odd number of the two letter sequence 105102 OR 
+    an even number of the five letter sequence 119104105108101 }
+    
+       
+    Your output should consist of
+    1. A sequence of states that the machine goes through,
+    2. An output of ACCEPT or REJECT, depending on the input string
+
+************************************************************************************/
+
+
+
+public class project{
+
+
+
+
+
+    
+}
+
+
+
+
+
+
